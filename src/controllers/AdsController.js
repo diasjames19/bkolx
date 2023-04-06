fb4267dc97d747852f864bf6f6ca0b2dceb4aaad
diasjames19 <diasjames@hotmail.com>
2023-04-06 10:30:15 -0300

    Criando anucio

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -34,8 +34,71 @@ module.exports = {
     },
     addAction:async(req, res)=>{
 
-        
+        let {title, price, priceneg, desc, cat, token} = req.body;
+        const user = await User.findOne({token}).exec();
+        if(!title || !cat){
+            res.jason({error:'Titulo e/ou categoria não froram preenchido! '});
+            return;
+        }
+        if(cat.length < 24){
+            res.json({error:'ID da categoria invalido!'});
+            return;
+        }
+        const category = await Category.findById(cat);
 
+        if(!category){
+            res.jason({error:'Categoria não existe!'});
+            return;
+        }
+        if(price){
+            price = price
+            .replace('.', '')
+            .replace(',', '.')
+            .replace('R$ ', '');
+           price = parseFloat(price)
+
+        }else{
+            price=0;
+        }
+        const newAd = new Ad();
+        newAd.status = true;
+        newAd.idUser = user._id;
+        newAd.state = user.state;
+        newAd.dateCreated = new Date();
+        newAd.title = title;
+        newAd.category = cat;
+        newAd.price = price;
+        newAd.priceNegotiable = (priceneg === 'true')?true : false;
+        newAd.description = desc;
+        newAd.views = 0;
+        if(req.files && req.files.img){
+            if(['image/jpeg','image/jpg','image/png'].includes(req.files.img.mimetype)){
+                let urlNewName = await addImage(req.files.img.data);
+                newAd.images.push({
+                    urlNewName,
+                    default:false
+                });
+            }
+         else{
+                for(let index = 0; index < req.files.img.length; index++){
+                    if(['image/jpeg','image/jpg','image/png'].includes(req.files.img[index].mimetype)){
+                        let urlNewName = await addImage(req.files.img[index].data);
+                        newAd.images.push({
+                            urlNewName,
+                            default:false
+                        });
+                    }
+
+
+                }
+            }
+        }
+        if(newAd.images.length > 0){
+            newAd.images[0].default = true;
+        }
+
+        const info = await newAd.save();
+        res.json({id:info._id});
     },
     getList:async(req, res)=>{
 
